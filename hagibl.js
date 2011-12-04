b63eac1729588df6f33ba16fb8c9efb92ac2bc6f
bbrodriges <bender@rodriges.org>
2011-12-04 22:44:20 +0400

    All new page generation methods. Full ajax without page reload. Page generation speed boost. Code minification and unification.

--- hagibl.js
+++ hagibl.js
@@ -30,30 +30,58 @@ function array_search( haystack, needle ) { /* FIND POST IN DATABASE */
 			return false;
 }
 
-function createErrorPage( message ) { /* CREATE ERROR PAGE */
-	$( '#articles' ).html( message );
-}
-
-function getArticles( page, id ) { /* RETURN PAGE BY GIVEN TYPE AND ID */
+function getArticles() { /* RETURN PAGE BY GIVEN TYPE AND ID */
 	var hash = window.location.hash.split('/');
-	var blogArticles = '';
+	var Articles = '';
 	if( hash[1] == '' || window.location.hash == '' ) { // if no hash - main page
 		window.location.hash = '#!/';
 		$.each( Database.articles , function( id , article ) {
 			if( id < Database.articlesonpage ) {
-				blogArticles = blogArticles +
+				Articles = Articles +
 					'<div class="article" id="' + id + '">' +
 					'<h1 class="title">' + article.title + '</h1>' +
-					'<span class="date">Posted on <a href="javascript:" data-link="' + Crypto.SHA1( article.title ) + '" class="link">' + article.date + '</a>, with tags: ';
+					'<span class="date">Posted on <a href="#!/article/' + Crypto.SHA1( article.title ) + '" data-link="' + Crypto.SHA1( article.title ) + '" class="link">' + article.date + '</a>, with tags: ';
 							$.each( article.tags , function( id, tag ) {
-								blogArticles = blogArticles + ' <a href="javascript:" data-tag="' + tag + '" class="tag">' + tag + '</a>';
+								Articles = Articles + ' <a href="#!/tag/' + tag + '" data-tag="' + tag + '" class="tag">' + tag + '</a>';
 							});
-				var articleText = parse_text( article.text, false ); 
-				blogArticles = blogArticles + '</span><div class="text">' + articleText + '</div></div>';
+				Articles = Articles + '</span><div class="text">' + parse_text( article.text, false ) + '</div></div>';
+			}
+		});
+	} else if( hash[1] == 'tag' ) { // if 'tag' in hash - building tag based posts search
+			$.each( Database.articles , function( id , article ) {
+				if( array_search( article.tags , decodeURI( hash[2] ) ) ) {
+					Articles = Articles +
+						'<div class="article" id="' + id + '">' +
+							'<h1 class="title">' + article.title + '</h1>' +
+							'<span class="date">Posted on <a href="javascript:" data-link="' + Crypto.SHA1( article.title ) + '" class="link">' + article.date + '</a>, with tags: ';
+						$.each( article.tags , function( id, tag ) {
+							Articles = Articles + ' <a href="javascript:" data-tag="' + tag + '" class="tag">' + tag + '</a>';
+						});
+					Articles = Articles + '</span><div class="text">' + parse_text( article.text, false ) + '</div></div>';
+				}
+			});
+	} else if( hash[1] == 'article' ) {
+		$.each( Database.articles , function( id , article ) {
+			if( Crypto.SHA1( article.title ) == hash[2] ) {
+				$( 'title' ).text( $( 'title' ).text() + ' / ' + article.title );
+				Articles = Articles +
+					'<div class="article" id="' + id + '">' +
+						'<h1 class="title">' + article.title + '</h1>' +
+						'<span class="date">Posted on <a href="javascript:" data-link="' + hash[2] + '" class="link">' + article.date + '</a>, with tags: ';
+					$.each( article.tags , function( id, tag ) {
+						Articles = Articles + ' <a href="javascript:" data-tag="' + tag + '" class="tag">' + tag + '</a>';
+					});
+					Articles = Articles + '</span><div class="text">' + parse_text( article.text, true ) + '</div></div>';
+				return false;
 			}
 		});
 	}
-	$( '#articles' ).html( blogArticles );
+
+	if( Articles == '' ) {
+			Articles = '<div class="article"><h1 class="title">Error</h1><div class="text">Something went wrong. Either no posts found or database error. Try to search on <a href="/">front page</a>.</div></div>';
+	}
+
+	$( '#articles' ).html( Articles );
 }
 
 /* ------------------------------------------------------- */
@@ -68,109 +96,41 @@ $.ajax({ /* READING DATABASE */
 });
 
 $( document ).ready(function() { /* WAITING PAGE TO BE LOADED TO DISPLAY ARTICLES */
-	if( Database != null ) {
+
+	if( Database != null ) { /* Loading articles */
 		getArticles();
 	} else {
 		createErrorPage( 'Database error' );
 	}
+
+	$("#stream").lifestream({ /* Loading lifestream */
+		list:[
+			{
+				service: "github",
+				user: "bbrodriges"
+			}
+		]
+	});
+
+	$( 'a.show-cut' ).live('click', function(){ /* Show cutted text on click */
+		$( 'div.cut' ).hide('medium');
+		$( 'a.show-cut' ).show('medium');
+		$(this).next( 'div.cut' ).show('medium');
+		$(this).hide();
+	});
+
+	$( '.date a' ).live('click', function(){
+		if( $(this).hasClass('tag') ) {
+			window.location.hash = '#!/tag/' + $(this).data('tag');
+		} else if( $(this).hasClass('link') ) {
+			window.location.hash = '#!/article/' + $(this).data('link');
+		}
+		getArticles();
+	});
+
+	$( '#title a' ).live('click', function(){
+		window.location.hash = '#!/';
+		getArticles();
+	});
+
 });
-
-		/*function getArticles( page ) {
-				var blogArticles = '';
-				$.each( Database.articles , function( id , article ) {
-					if( id < Database.articlesonpage ) {
-						blogArticles = blogArticles +
-							'<div class="article" id="' + id + '">' +
-								'<h1 class="title">' + article.title + '</h1>' +
-								'<span class="date">Posted on <a href="javascript:" data-link="' + Crypto.SHA1( article.title ) + '" class="link">' + article.date + '</a>, with tags: ';
-						$.each( article.tags , function( id, tag ) {
-							blogArticles = blogArticles + ' <a href="javascript:" data-tag="' + tag + '" class="tag">' + tag + '</a>';
-						});
-						var articleText = parse_text( article.text, false ); 
-						blogArticles = blogArticles + '</span><div class="text">' + articleText + '</div></div>';
-					} else {
-						return false;
-					}
-				});
-				$( '#articles' ).html( blogArticles );
-		}
-
-		function getArticlesByTag( tag ) {
-			var tagArticles = '';
-			$.each( Database.articles , function( id , article ) {
-				if( array_search( article.tags , decodeURI( tag ) ) ) {
-					tagArticles = tagArticles +
-						'<div class="article" id="' + id + '">' +
-							'<h1 class="title">' + article.title + '</h1>' +
-							'<span class="date">Posted on <a href="javascript:" data-link="' + Crypto.SHA1( article.title ) + '" class="link">' + article.date + '</a>, with tags: ';
-						$.each( article.tags , function( id, tag ) {
-							tagArticles = tagArticles + ' <a href="javascript:" data-tag="' + tag + '" class="tag">' + tag + '</a>';
-						});
-						var articleText = parse_text( article.text, false ); 
-						tagArticles = tagArticles + '</span><div class="text">' + articleText + '</div></div>';
-				}
-			});
-			if( tagArticles == '' ) {
-				message = '<div class="article"><h1 class="title">No posts with this tag found</h1><div class="text">You can try to search on <a href="/">front page</a>.</div></div>';
-				createErrorPage( message );
-			} else {
-				$( '#articles' ).html( tagArticles );
-			}
-		}
-
-		function getArticleById( articlehash ) {
-			var Article = '';
-			$.each( Database.articles , function( id , article ) {
-				if( Crypto.SHA1( article.title ) == articlehash ) {
-					$( 'title' ).text( $( 'title' ).text() + ' / ' + article.title );
-					Article = Article +
-						'<div class="article" id="' + id + '">' +
-							'<h1 class="title">' + article.title + '</h1>' +
-							'<span class="date">Posted on <a href="javascript:" data-link="' + articlehash + '" class="link">' + article.date + '</a>, with tags: ';
-						$.each( article.tags , function( id, tag ) {
-							Article = Article + ' <a href="javascript:" data-tag="' + tag + '" class="tag">' + tag + '</a>';
-						});
-						var articleText = parse_text( article.text, true ); 
-						Article = Article + '</span><div class="text">' + articleText + '</div></div>';
-					return false;
-				}
-			});
-			if( Article == '' ) {
-				message = '<div class="article"><h1 class="title">Post has not been fount</h1><div class="text">You can try to search on <a href="/">front page</a>.</div></div>';
-				createErrorPage( message );
-			} else {
-				$( '#articles' ).html( Article );
-			}
-		}
-
-		$( document ).ready(function() {
-
-			if( Database == null ) {
-				message = '<div class="article"><h1 class="title">Database broken</h1><div class="text">Something wrong with your JSON database. You can validate database at <a href="http://jsonlint.com">JSONlint</a>.</div></div>';
-				createErrorPage( message );
-			}
-
-			$( 'a.show-cut' ).live('click', function(){
-				$( 'div.cut' ).hide('medium');
-				$( 'a.show-cut' ).show('medium');
-				$(this).next( 'div.cut' ).show('medium');
-				$(this).hide();
-			});
-
-			$( '.date a' ).live('click', function(){
-				if( $(this).hasClass('tag') ) {
-					window.location.search = 'tag=' + $(this).data('tag');
-				} else if( $(this).hasClass('link') ) {
-					window.location.search = 'article=' + $(this).data('link');
-				}
-			});
-			
-			$("#stream").lifestream({
-				list:[
-				  {
-					service: "github",
-					user: "bbrodriges"
-				  }
-				]
-			});
-		});*/
